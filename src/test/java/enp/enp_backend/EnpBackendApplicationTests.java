06d6b086a2973ea4a95c8c083fe798cf2d429722
NatchaPetchsikul <natcha_pet@cmu.ac.th>
2023-05-28 14:43:19 +0700

    initial project

---
+++ src/test/java/enp/enp_backend/EnpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package enp.enp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
